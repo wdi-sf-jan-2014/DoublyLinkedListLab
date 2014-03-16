7d94c2e706fb5da630e2dfb2d3d860fd15ddb859
Tim Garcia <tim.m.garcia@gmail.com>
2014-03-16 12:18:06 -0700

    Adding specs for the bracket, '[]', accessor.

--- list_spec.rb
+++ list_spec.rb
@@ -33,4 +33,31 @@ describe List do
     expect(list.pop).to be_nil
     expect(list.length).to eq(0)
   end
+
+  it 'should respond to [] accessors' do
+    expect(list).to respond_to(:[]).with(1).argument
+    expect(list).to respond_to(:[]=).with(2).argument
+  end
+
+  it 'returns nil if the index is out of bounds' do
+    expect(list[-99]).to be_nil
+    expect(list[0]).to be_nil
+    list.push(10)
+    expect(list[1]).to be_nil
+  end
+
+  it 'returns the value at the given index' do
+    list.push(1)
+    list.push(2)
+    expect(list[0]).to eq(1)
+    expect(list[1]).to eq(2)
+  end
+
+  it 'reassigns a value at a given index' do
+    list.push(1)
+    expect(list[0]).to eq(1)
+
+    list[0] = 5
+    expect(list[0]).to eq(5)
+  end
 end
