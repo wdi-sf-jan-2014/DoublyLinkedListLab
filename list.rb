bd7c06de066b0792688c3de2df0cae1f06e057a4
Tim Garcia <tim.m.garcia@gmail.com>
2014-03-16 11:49:35 -0700

    An initial solution for push, pop and length of the list

--- list.rb
+++ list.rb
@@ -17,7 +17,11 @@
 # after the previous methods are implemented:
 
 # reverse, max, min, reduce, each, flatten
+
+require './node.rb'
+
 class List
+  attr_reader :length
 
   def initialize
     @head = nil
@@ -27,11 +31,48 @@ class List
 
   # Adds a new value to the end of the list.
   def push(value)
+    if @head == nil
+      @head = Node.new(value)
+      @tail = @head
+    else
+      old_tail = @tail
+      @tail = Node.new(value)
+      old_tail.next = @tail
+      @tail.previous = old_tail
+    end
+    @length += 1
+    nil
   end
 
   # Returns the value that is popped off
   # or nil if none exists
   def pop
+    return nil if @length == 0
+
+    # special case where the length of the list is 1
+    # so the head and tail need to be set to nil
+    if @length == 1
+      return_value = @tail.value
+      nil_node(@tail)
+      @tail = nil
+      @head = nil
+      @length = 0
+      return return_value
+    end
+
+    old_tail = @tail
+    @tail = old_tail.previous
+    @tail.next = nil
+
+    return_value = old_tail.value
+
+    @length -= 1
+
+    # Making sure the old_tail has no references to nodes or
+    # values.
+    nil_node(old_tail)
+
+    return_value
   end
 
   def insert(value)
@@ -60,5 +101,12 @@ class List
   end
 
   def last
+  end
+
+private
+  def nil_node(node)
+    node.value = nil
+    node.previous = nil
+    node.next = nil
   end
 end
