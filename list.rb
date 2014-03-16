87d6116d788178f0a3f23d9f3e6009366bab224e
Tim Garcia <tim.m.garcia@gmail.com>
2014-03-16 10:39:07 -0700

    Adding all the basic methods to the List class.  The methods we expect them to implement are initialize, push, pop, insert, [], []=, shift, unshift, first and last.  The comments adds information on the order of operations for implementing the methods.  There is also some bonus methods that can be implemented

--- list.rb
+++ list.rb
@@ -1,2 +1,64 @@
+# A List class that should be implemented as
+# a doubly linked list.  Also, it is important
+# to keep track of a head and tail pointer
+# for this lab.
+
+# The following methods are the most important
+# to implement and should be done first:
+
+# new, push, pop, and length
+
+# The following methods are nice to have and should
+# be implemented next:
+
+# [], []=, insert, shift, unshift, first, last
+
+# The following methods are bonus and should only be tackled
+# after the previous methods are implemented:
+
+# reverse, max, min, reduce, each, flatten
 class List
+
+  def initialize
+    @head = nil
+    @tail = nil
+    @length = 0
+  end
+
+  # Adds a new value to the end of the list.
+  def push(value)
+  end
+
+  # Returns the value that is popped off
+  # or nil if none exists
+  def pop
+  end
+
+  def insert(value)
+  end
+
+  # Given an index, returns the value at that index
+  def [](index)
+  end
+
+  # Sets a value at the given index.  Returns the value that
+  # was assigned
+  def []=(index, value)
+    value
+  end
+
+  # Returns the first value that was removed from the list and
+  # shifts all items down by 1
+  def shift
+  end
+
+  # Adds a new value to the front of the list
+  def unshift(value)
+  end
+
+  def first
+  end
+
+  def last
+  end
 end
